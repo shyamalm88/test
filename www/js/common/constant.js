ec6758e89d61bc8f74d4be08076cb7097df8bfc6
Arghya Majumder <shyamalm88@gmail.com>
2016-05-25 12:08:57 +0000

    shyamal updated

--- www/js/common/constant.js
+++ www/js/common/constant.js
@@ -1,7 +1,7 @@
 'use strict'
 hereApp.constant('hereAppConstant',{
 	'WSPATH':{
-		'PATH':'http://localhost:3333/'
+		'PATH':'https://iamhere-shyamalm88.c9users.io:8081/'
 	},
 	'WSURL':{
 		'GET_PLACE_AUTOCOMPLETE':'webService/getPlaceAutoComplete',
