3e52c2eb3cb7c68c6bbfcbf25d6179591ce70f50
shyamalm88 <shyamalm88@gmail.com>
2016-05-27 01:43:20 +0530

    loading section added

--- www/js/common/commonController.js
+++ www/js/common/commonController.js
@@ -15,7 +15,7 @@ hereApp.controller('commonController', ['$scope', '$state', 'hereAppConstant', '
         };
 
         $ionicLoading.show({
-            template: '<img src="img/bloader.gif" alt="" />'
+            template: '<img src="img/bloader.svg" alt="" />'
         })
 
         $scope.toggleGroup = function(group) {
